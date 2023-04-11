ae0eea476ebd36db679d8618a7b36033e149ce4c
David Tauraso <david318@me.com>
2023-04-11 05:43:16 -0700

    Updated code

--- ContextualStateChart/ContextualStateChartTypes.go
+++ ContextualStateChart/ContextualStateChartTypes.go
@@ -1,5 +1,32 @@
 package ContextualStateChartTypes
 
 type Graph struct {
-	
+}
+
+type Value struct {
+	IntValue    int    `json:"intValue",omitvalue`
+	StringValue string `json:"stringValue",omitvalue`
+}
+type State struct {
+	ID                  int
+	name                []string
+	FunctionCode        func(Graph) bool
+	EdgeKinds           map[string]Edges
+	Parents             map[string]int
+	StartChildren       []int
+	HaveStartChildren   bool
+	Value               Value
+	arrayValues         []int          `json:"arrayValues",omitvalue`
+	mapValues           map[string]int `json:"arrayValues",omitvalue`
+	LockedByStates      map[string]bool
+	LockedByStatesCount int
+	// Database            map[string]IStateNamePartTree
+}
+type Edges struct {
+	Edges       [][]string
+	AreParallel bool
+}
+type IDatabase struct {
+	Array []any
+	Map   map[string]any
 }
