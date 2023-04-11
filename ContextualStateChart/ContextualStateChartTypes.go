072181f207b2bb94ca5c790f66f772a6df190430
David Tauraso <david318@me.com>
2023-04-11 06:03:14 -0700

    Updated code

--- ContextualStateChart/ContextualStateChartTypes.go
+++ ContextualStateChart/ContextualStateChartTypes.go
@@ -15,6 +15,17 @@ type Edges struct {
 
 // Parents: NDParentStateName -> ID
 // MapValues: 1D string -> ID
+/*
+local variable
+1D name -> primitive value
+1D name -> array of ID's
+1D name -> map of string keys -> ID's
+
+database variable
+ND name -> primitive value
+ND name -> array of ID's
+ND name -> map of string keys -> ID's
+*/
 type State struct {
 	ID                  int              `json:"ID"`
 	Name                []string         `json:"name"`
