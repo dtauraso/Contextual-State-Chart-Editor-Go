f6c5552901579fed9f691945654288143f788d6f
David Tauraso <david318@me.com>
2023-04-11 05:55:19 -0700

    Updated code

--- ContextualStateChart/ContextualStateChartTypes.go
+++ ContextualStateChart/ContextualStateChartTypes.go
@@ -4,23 +4,22 @@ type Graph struct {
 }
 
 type Value struct {
-	IntValue    int    `json:"intValue",omitvalue`
-	StringValue string `json:"stringValue",omitvalue`
+	IntValue    int    `json:"intValue,omitempty"`
+	StringValue string `json:"stringValue,omitempty"`
 }
 type State struct {
-	ID                  int
-	name                []string
-	FunctionCode        func(Graph) bool
-	EdgeKinds           map[string]Edges
-	Parents             map[string]int
-	StartChildren       []int
-	HaveStartChildren   bool
-	Value               Value
-	arrayValues         []int          `json:"arrayValues",omitvalue`
-	mapValues           map[string]int `json:"arrayValues",omitvalue`
-	LockedByStates      map[string]bool
-	LockedByStatesCount int
-	// Database            map[string]IStateNamePartTree
+	ID                  int              `json:"ID"`
+	Name                []string         `json:"name"`
+	FunctionCode        func(Graph) bool `json:"FunctionCode,omitempty"`
+	EdgeKinds           map[string]Edges `json:"EdgeKinds,omitempty"`
+	Parents             map[string]int   `json:"Parents,omitempty"`
+	StartChildren       []int            `json:"StartChildren,omitempty"`
+	HaveStartChildren   bool             `json:"HaveStartChildren,omitempty"`
+	Value               Value            `json:"Value,omitempty"`
+	ArrayValues         []int            `json:"arrayValues,omitempty"`
+	MapValues           map[string]int   `json:"mapValues,omitempty"`
+	LockedByStates      map[string]bool  `json:"LockedByStates,omitempty"`
+	LockedByStatesCount int              `json:"LockedByStatesCount,omitempty"`
 }
 type Edges struct {
 	Edges       [][]string
