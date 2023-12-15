11077a9f4d6b397c8807f7c15b121a9dabee4994
David Tauraso <david318@me.com>
2023-12-14 20:44:54 -0800

    Updated code

--- Blocks.go
+++ Blocks.go
@@ -1,6 +1,22 @@
 package main
 
-type BlockContext struct {
-	Id   int    `json:"Id"`
-	Name string `json:"Name"`
+import ContextualStateChartTypes "Contextual-State-Chart-Editor-Go/ContextualStateChart"
+
+type Parent struct {
+	IdList           []int `json:"IdList,omitempty"`
+	PositionInParent int   `json:"PositionInParent,omitempty"`
+}
+
+type Variable struct {
+	Value   ContextualStateChartTypes.Atom   `json:"Value,omitempty"`
+	History []ContextualStateChartTypes.Atom `json:"History,omitempty"`
+}
+
+type Block struct {
+	Id          int                 `json:"Id"`
+	Name        string              `json:"Name,omitempty"`
+	Parents     map[int]Parent      `json:"Parents,omitempty"`
+	Sequence    [][]int             `json:"Sequence,omitempty"`
+	Variables   map[string]Variable `json:"Variables,omitempty"`
+	NextContext map[string]int      `json:"NextContext,omitempty"`
 }
