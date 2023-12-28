a59a1ff979a53b3e8c59a728c7a5e454657cd3c4
David Tauraso <david318@me.com>
2023-12-28 13:24:18 -0800

    Updated code

--- ContextualStateChart/ContextualStateChartTypes.go
+++ ContextualStateChart/ContextualStateChartTypes.go
@@ -14,6 +14,58 @@ import (
 	"time"
 )
 
+type LinkedNode struct {
+	Prev int
+	Curr int
+	Data Link
+}
+
+type LinkedList struct {
+	LinkedList  []LinkedNode
+	FirstNode   int
+	LastNode    int
+	CurrentNode int
+}
+
+type Parent struct {
+	Link             Link `json:"Link,omitempty"`
+	PositionInParent int  `json:"PositionInParent,omitempty"`
+}
+
+type Change struct {
+	Value Atom   `json:"Value,omitempty"`
+	Type  string `json:"Type,omitempty"`
+}
+
+type Variable struct {
+	Value   Atom     `json:"Value,omitempty"`
+	History []Change `json:"History,omitempty"`
+}
+
+type Link struct {
+	Ids                       []string  `json:"Ids,omitempty"`
+	UsageCount                int       `json:"UsageCount,omitempty"`
+	TimeLastUsed              time.Time `json:"TimeLastUsed,omitempty"`
+	ActiveConnectionLastIndex int       `json:"ActiveConnectionLastIndex,omitempty"`
+	ChildBlockLink            []string  `json:"ChildBlockLink,omitempty"`
+}
+
+/*func(map[string]Block, []string) bool*/
+type Block struct {
+	Id          string              `json:"Id"`
+	Parents     []Parent            `json:"Parents,omitempty"`
+	Sequence    LinkedList          `json:"Link,omitempty"`
+	Variables   map[string]Variable `json:"Variables,omitempty"`
+	NestedBlock map[string]Block    `json:"NestedBlock,omitempty"`
+}
+
+type Blocks struct {
+	Blocks map[string]Block `json:"Blocks,omitempty"`
+	MaxInt int              `json:"MaxInt,omitempty"`
+}
+
+var blocks Blocks
+
 // Parents: NDParentStateName -> Id
 // MapValues: 1D string -> Id
 /*
@@ -781,6 +833,8 @@ func runGoroutines(node *Node, wg *sync.WaitGroup) {
 
 func Pattern() {
 
+	// myBlocks := make(Blocks)
+	// myBlocks.Blocks["if"] = Block{Id: "if"}
 	// sequence of blocks for different directions
 	// all spirals have to be larger than 1 unit spiral
 
